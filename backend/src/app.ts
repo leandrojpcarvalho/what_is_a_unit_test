5615268362e182db5241ead9e9ebc8f11edd409e
Leandro <ljpcarvalho@gmail.com>
2024-02-10 15:53:40 -0300

    backend<build>: config app with node

---
+++ backend/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const PORT = 3002;
+
+const server = express();
+server;
+
+server.use('/', (req, res) => res.send('Bem vinde!'));
+
+server.listen(PORT, () =>
+  console.log(`Servidor rodando em node na porta:${PORT}`)
+);
