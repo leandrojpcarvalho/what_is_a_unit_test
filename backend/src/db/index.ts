80aae1684cb833ebcaff73dc81dba1b11366e4d6
Leandro <ljpcarvalho@gmail.com>
2024-02-13 16:18:43 -0300

    backend<refact>: Make the instance of sqlite singleton and remove the instance of memory

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,14 +1,32 @@
 import SqLite3 from 'sqlite3';
 import ITasks from '../interface/ITasks';
 import Tasks from './model/tasks';
+import { Creation } from '../types/generics';
+import { CreationTask } from '../types/database';
+
+type ListOfTasks = CreationTask[];
+
+const defaultTasks: ListOfTasks = [
+  { description: 'implementar Base de dados SQL com Docker' },
+  { description: 'implementar ORM sequelize' },
+  { description: 'Implementar ORM Prisma' },
+];
 
 export default class Sqlite {
+  public static instance: Sqlite | null = null;
   public db: SqLite3.Database;
 
   constructor() {
-    this.db = new SqLite3.Database(':memory:');
+    this.db = new SqLite3.Database('./src/db/db.db');
     this.seedAll();
     this.listen();
+  }
+
+  public static getInstance(): Sqlite {
+    if (!Sqlite.instance) {
+      Sqlite.instance = new Sqlite();
+    }
+    return Sqlite.instance;
   }
 
   seedAll() {
@@ -23,33 +41,69 @@ export default class Sqlite {
 
       this.db.run(table);
 
-      const date = '08-10-1999';
-      const stmp = this.db.prepare(
-        `INSERT INTO tasks (description, created_at, updated_at) VALUES (?, ?, ?)`
-      );
-      for (let i = 0; i < 3; i++) {
-        stmp.run(`task ${i + 1}`, date, date);
-      }
-      stmp.finalize();
+      this.db.all('SELECT * FROM tasks', (error, result) => {
+        if (error) {
+          console.log(error);
+        } else if (result.length < 3) {
+          this.bulkInsertTasks(defaultTasks);
+        }
+      });
     });
   }
 
-  async run(sql: string) {
-    return this.db.run(sql);
+  bulkInsertTasks(data: ListOfTasks) {
+    const stmt = this.db.prepare(
+      `INSERT INTO tasks (description, created_at, updated_at) VALUES (?, ?, ?)`
+    );
+    const date = new Date().toISOString().split('T')[0];
+    for (let i = 0; i < data.length; i++) {
+      const { description, createdAt, updatedAt } = data[i];
+      if (createdAt && updatedAt) {
+        stmt.run(description, createdAt, updatedAt);
+      } else {
+        stmt.run(description, date, date);
+      }
+    }
+    stmt.finalize();
+  }
+
+  async run(sql: string, value: CreationTask) {
+    return new Promise<ITasks[] | Error>((resolve, reject) => {
+      this.db.run(
+        sql,
+        Object.values(value),
+        (error: string, result: string) => {
+          if (error) {
+            reject(error);
+          } else {
+            if (!result) {
+              this.db.all<ITasks>('SELECT * FROM tasks', (error, rows) => {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve(rows);
+                }
+              });
+            }
+          }
+        }
+      );
+    });
   }
 
   async all(sql: string) {
     return new Promise<ITasks[] | Error>((resolve, reject) =>
-      this.db.all<ITasks>(sql, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
+      this.db.serialize(() => {
+        this.db.all<ITasks>(sql, (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        });
       })
     );
   }
-
   private async listen() {
     this.db.all('SELECT * FROM tasks', (error: string, result: string) => {
       if (error) {
